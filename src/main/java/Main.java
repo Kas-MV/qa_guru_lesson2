700ac598f1d2ebf53739ab6865ffc74dc17bcc90
Kas-MV <kasimov.mv@mail.ru>
2023-05-26 15:48:25 +0400

    var added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, qa.guru!!!!");
+        String message = "Hello, qa.guru!!!!";
+        System.out.println(message);
     }
 }
